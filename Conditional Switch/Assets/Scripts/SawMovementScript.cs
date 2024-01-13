a125555018b0778cbd857cd5c1524aad5ad80a01
rainpaw <139286499+rainpaw@users.noreply.github.com>
2024-01-13 08:27:15 -0600

    Changed Saw Speed + Destroy

--- Conditional Switch/Assets/Scripts/SawMovementScript.cs
+++ Conditional Switch/Assets/Scripts/SawMovementScript.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SawMovementScript : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    private float moveSpeed;
     public float rotationSpeed = 480f;
+
+    private void Start()
+    {
+        moveSpeed = Random.Range(1, 14);
+    }
 
     private void Update()
     {
         transform.position += (Vector3.left * moveSpeed) * Time.deltaTime;
         transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y, (1 * rotationSpeed) * Time.deltaTime));
+
+        if (transform.position.x < -20)
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
